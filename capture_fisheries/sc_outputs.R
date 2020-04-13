5083ebd9c89c873e5122fa76ae6c8432d0cce8f9
traceybit <traceymangin@gmail.com>
2020-04-13 12:20:41 -0700

    added notes for user

--- capture_fisheries/sc_outputs.R
+++ capture_fisheries/sc_outputs.R
@@ -1,37 +1,32 @@
 ## Tracey Mangin
 ## May 31, 2019
-## Create capture outputs for use in creating aggregate supply curve
+## Future of Food From the Sea
+
+## create aggregated supply curves
+
+## Make sure to run files in init_cond folder before proceeding.
+
+## This is STEP 3 for the marine capture fishery analysis.
+
 
 ## attach librariess
 library(tidyverse)
 library(viridis)
-# library("wesanderson")
-# library(janitor)
-# library(scales)
-# library(plotly)
-# library(rgdal)
-
-## bp 
-bp_path <- "/Volumes/GoogleDrive/Shared\ drives/emlab/projects/current-projects/blue-paper-1/project-materials/nature-revision/"
-
-
-# save path for figures
-savepath <- "~/Box/SFG Centralized Resources/Projects/Ocean Protein/Outputs/Capture/"
-
-## set pathstart
-pathstart <- "~/Box/SFG Centralized Resources/Projects/Ocean Protein/Outputs/Capture/data/final/"
 
 ## upsides msy
-upsides <- read.csv("~/Box/SFG Centralized Resources/Projects/Upsides/upside-share/ProjectionData.csv", stringsAsFactors = F)
+## note to user: update the path and load ProjectionData.csv
+upsides <- read.csv("/ffts/m_capture_data/ProjectionData.csv", stringsAsFactors = F)
 
 ## conversion factors
-conv_vals <- read_csv("bp1_nature/processed_data/isscaap_food_conv_all.csv")
+## note to user: update the path and load conversion file created in init_cond/edible_conv.R script.
+conv_vals <- read_csv("isscaap_food_conv_all.csv")
 
 ## scale for reduction fisheries
 red_scale_mult <- 1 - 0.18
 
 ## get fao scale value
-fao_scale_df <- read_csv(paste0(bp_path, "outputs/upside_fao_scale_df.csv"))
+## note to user: update the path and load conversion file created in init_cond/init_conditions.R script.
+fao_scale_df <- read_csv("upside_fao_scale_df.csv")
 
 ## scale value
 cf_scale_val <- fao_scale_df %>%
@@ -51,16 +46,20 @@ sum(upsides0$msy_food)
 
 
 ## read in relevant outputs/processed data
-prod_df <- readRDS(paste0(pathstart, "production_df_smry.rds"))
-prod_s1_df <- readRDS(paste0(pathstart, "production_s1_smry.rds"))
-prod_s2_df <- readRDS(paste0(pathstart, "production_s2_smry.rds"))
-prod_s3_df <- readRDS(paste0(pathstart, "production_s3_smry.rds"))
+## note to user: these are produced in STEP 2 of the marine capture analysis (capture_sc_run.R)
+## update path and load files created during STEP 2
+prod_df <- readRDS("production_df_smry.rds")
+prod_s1_df <- readRDS("production_s1_smry.rds")
+prod_s2_df <- readRDS("production_s2_smry.rds")
+prod_s3_df <- readRDS("production_s3_smry.rds")
 
-prod_fmsy_df <- readRDS(paste0(pathstart, "fmsy_prod_smry.rds")) %>%
+
+prod_fmsy_df <- readRDS("fmsy_prod_smry.rds") %>%
   mutate(scenario = "FMSY_orig_inputs")
 
-prod_f0_df <- readRDS(paste0(pathstart, "f0_prod_smry.rds")) %>%
+prod_f0_df <- readRDS("f0_prod_smry.rds") %>%
   mutate(scenario = "F0_orig_inputs")
+
 
 ## combine, summarise at price level
 sc_capture_df <- rbind(prod_df, prod_s1_df, prod_s2_df, prod_s3_df, prod_fmsy_df, prod_f0_df) %>%
@@ -105,8 +104,8 @@ sc_si <- ggplot(sc_capture_df_sum %>% filter(price <= 5000,
         axis.text = element_text(size = 18),
         axis.title = element_text(size = 20))
 
-
-ggsave(filename =  paste0(bp_path, "figures/si_capture.png"), sc_si, width = 10, height = 8, units = "in", dpi = 300)
+## note to user: this figure is produced for the SI. Update path and save.
+ggsave(filename =  "si_capture.png", sc_si, width = 10, height = 8, units = "in", dpi = 300)
 
 
 ## scenario, price, sector, capture, conversion_grp, conversion_value, mt
@@ -116,9 +115,10 @@ sc_capture_df2 <- sc_capture_df %>%
 
 
 
-## save file for mariculture
-write.csv(sc_capture_df2, paste0(savepath, "data/final/harvest_x_price_x_conversion.csv"))
-write.csv(sc_capture_df2, paste0(bp_path, "outputs/harvest_x_price_x_conversion.csv"))
+## save file for mariculture analysis -- this file is used to create file with supply curves
+## from both mariculture and marine capture.
+## update the path and save
+write.csv(sc_capture_df2, "harvest_x_price_x_conversion.csv")
 
 
 
