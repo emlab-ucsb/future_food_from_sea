8ab22ea448d45a20bcb09457ea9780f124dd4d56
traceybit <traceymangin@gmail.com>
2020-05-29 10:10:37 -0700

    finalize figure

--- analysis/donut_fig.R
+++ analysis/donut_fig.R
@@ -18,6 +18,11 @@ final_indiv_df <- read_csv("sector_sp_prod_final.csv") %>%
 ## note to user: change path, load file created in init_conditions.R
 init_pq_df <- read_csv("init_pq_df.csv")
 
+## initial production and price, harvest
+## note to user: change path, load file created in init_conditions.R
+init_pq_df <- read_csv(paste0(bp_path, "outputs/init_pq_df.csv"))
+
+
 
 donut_df <- final_indiv_df %>%
   group_by(demand) %>%
@@ -66,9 +71,9 @@ donut_df3$sector <- factor(donut_df3$sector,
                            levels = c("Marine wild fisheries", "Finfish mariculture", "Bivalve mariculture"))
 
 donut_df4 <- donut_df3 %>%
-  mutate(demand = ifelse(demand == "current", "2050 under current demand",
-                         ifelse(demand == "future", "2050 under future demand",
-                                ifelse(demand == "extreme", "2050 under extreme demand", "Initial production")))) %>%
+  mutate(demand = ifelse(demand == "current", "b 2050 under current demand",
+                         ifelse(demand == "future", "c 2050 under future demand",
+                                ifelse(demand == "extreme", "d 2050 under extreme demand", "a Initial production")))) %>%
   mutate(label = paste0(round(rel_prod * 100), "%")) %>%
   group_by(demand) %>%
   arrange(desc(sector)) %>%
@@ -82,14 +87,14 @@ donut_df4 <- donut_df3 %>%
 max_prod <- max(donut_df4$tot_prod)
 
 donut_df4$demand <- factor(donut_df4$demand,
-                           levels = c("Initial production", "2050 under current demand", "2050 under future demand", "2050 under extreme demand"))
+                           levels = c("a Initial production", "b 2050 under current demand", "c 2050 under future demand", "d 2050 under extreme demand"))
 
 
-donut_fig <- ggplot(donut_df4, aes(x=1.7, y=rel_prod, fill=sector, width = tot_prod / max_prod)) +
+donut_fig <- ggplot(donut_df4, aes(x = 1.7, y=rel_prod, fill=sector, width = tot_prod / max_prod)) +
   geom_bar(position = 'fill', stat = 'identity', alpha = 1)  +
   # geom_label(x=2.3, aes(y=centers, label=label), fill = "none", color = "white", size=5, inherit.aes = F) +
-  geom_text(x = 2.5, aes(y=centers, label = label), size = 6) +
-  geom_text(x = .5,y=.8,size=6,aes(label=center_text)) +
+  geom_text(x = 2.5, aes(y=centers, label = label), size = 3) +
+  geom_text(x = .5, y=.8, size = 3,aes(label=center_text)) +
   facet_wrap(~ demand) + 
   xlim(0.5, 2.5) +
   coord_polar(theta = 'y') + 
@@ -102,15 +107,17 @@ donut_fig <- ggplot(donut_df4, aes(x=1.7, y=rel_prod, fill=sector, width = tot_p
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        legend.position = "top",
-        strip.text = element_text(size = 20),
-        legend.text = element_text(size = 20),
+        legend.position = "bottom",
+        legend.text = element_text(size = 7),
         legend.title = element_blank(),
         axis.text = element_blank(),
-        axis.ticks = element_blank())
+        axis.ticks = element_blank(),
+        strip.text = element_text(size = 8, hjust = 0),
+        strip.background = element_rect(color = "white", fill = "white"))
 
 ## note to user -- change path and save figure 5 in main text
-ggsave(filename =  "fig5.png", donut_fig, width = 10, height = 10, units = "in", dpi = 300)
+ggsave(filename = "fig5.pdf", donut_fig, width = 120, height = 120, units = "mm", dpi = 600)
+
 
 
 ## compare outputs to initial
